f6159831670a29d826c14392371a6efed8e5aee4
Juan-MZ <juanjosz@unicauca.edu.co>
2024-04-10 08:20:18 -0500

    person y docente

--- src/main/java/co/edu/unicauca/asae/assttaller/models/Docente.java
+++ src/main/java/co/edu/unicauca/asae/assttaller/models/Docente.java
@@ -1,4 +1,22 @@
 package co.edu.unicauca.asae.assttaller.models;
 
-public class Docente {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "docente")
+public class Docente extends Persona{
+    @Column(name = "correo", length = 30)
+    private String correo;
+    @Column(name = "vinculacion", length = 30)
+    private String vinculacion;
+    @Column(name = "departamento", length = 30)
+    private String departamento;
 }
---
+++ src/main/java/co/edu/unicauca/asae/assttaller/models/enums/TipoIdentificacion.java
@@ -0,0 +1,5 @@
+package co.edu.unicauca.asae.assttaller.models.enums;
+
+public enum TipoIdentificacion {
+    CC, CE, TI
+}
