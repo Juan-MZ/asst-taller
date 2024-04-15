464219602293f20480416183faeabe93dc9f0926
Juan-MZ <juanjosz@unicauca.edu.co>
2024-04-15 14:46:12 -0500

    nuevos cambios

--- src/main/java/co/edu/unicauca/asae/assttaller/models/Pregunta.java
+++ src/main/java/co/edu/unicauca/asae/assttaller/models/Pregunta.java
@@ -1,4 +1,7 @@
 package co.edu.unicauca.asae.assttaller.models;
 
 public class Pregunta {
+    private Integer idpregunta;
+    private String enunciado;
+
 }
--- src/main/java/co/edu/unicauca/asae/assttaller/models/TipoPregunta.java
+++ src/main/java/co/edu/unicauca/asae/assttaller/models/TipoPregunta.java
@@ -1,7 +1,17 @@
 package co.edu.unicauca.asae.assttaller.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "departamentos")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class TipoPregunta {
     private Integer idtippregunta;
     private String nombre;
-
+    private String descripcion;
 }
