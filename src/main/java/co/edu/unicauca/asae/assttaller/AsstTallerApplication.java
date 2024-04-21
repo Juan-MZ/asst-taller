dddba7a48500166fe4326199406629b609138c16
Juan-MZ <juanjosz@unicauca.edu.co>
2024-04-21 16:36:32 -0500

    adición de respuestas para el docente 2

--- src/main/java/co/edu/unicauca/asae/assttaller/AsstTallerApplication.java
+++ src/main/java/co/edu/unicauca/asae/assttaller/AsstTallerApplication.java
@@ -61,7 +61,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 		almacenarDepartamentos();
 		crearDocentes();
 		almacenarCuestionarios();
-		almacenarRespuestas(); 
+		almacenarRespuestas();
 	}
 
 	/**
@@ -101,7 +101,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 		Cuestionario objCuestionario1 = new Cuestionario();
 		objCuestionario1.setTitulo("Ficha de datos generales");
 		objCuestionario1.setDescripcion("Cuestionario de datos generales");
-		
+
 		TipoPregunta objTipoPregunta1 = new TipoPregunta();
 		objTipoPregunta1.setNombre("Datos generales");
 		objTipoPregunta1.setDescripcion("Preguntas de datos generales");
@@ -123,15 +123,15 @@ public class AsstTallerApplication implements CommandLineRunner {
 		objPregunta3.setEnunciado("¿Cuál es su ocupación o profesión");
 		objPregunta3.setObjTipoPregunta(objTipoPregunta1);
 		preguntasCuestionario1.add(objPregunta3);
-		
+
 		objCuestionario1.setPreguntas(preguntasCuestionario1);
 		objPregunta1.setObjCuestionario(objCuestionario1);
 		objPregunta2.setObjCuestionario(objCuestionario1);
 		objPregunta3.setObjCuestionario(objCuestionario1);
-		
+
 		this.servicioBDCuestionario.save(objCuestionario1);
 
-		/**------------------------------------------------------- */
+		/** ------------------------------------------------------- */
 		Cuestionario objCuestionario2 = new Cuestionario();
 		objCuestionario2.setTitulo("Cuestionario de factores de riesgo psicosocial intralaboral");
 		objCuestionario2.setDescripcion("Cuestionario sobre los factores de riesgo psicosocial intralaboral");
@@ -144,20 +144,20 @@ public class AsstTallerApplication implements CommandLineRunner {
 		List<Pregunta> preguntasCuestionario2 = new ArrayList<>();
 
 		Pregunta objPregunta4 = new Pregunta();
-		objPregunta4.setEnunciado("El ruido en el lugar donde trabajo es molesto");
+		objPregunta4.setEnunciado("El ruido en el lugar donde trabajo es molesto?");
 		objPregunta4.setObjTipoPregunta(objTipoPregunta2);
 		preguntasCuestionario2.add(objPregunta4);
 
 		Pregunta objPregunta5 = new Pregunta();
-		objPregunta5.setEnunciado("El espacio donde trabajo es comodo");
+		objPregunta5.setEnunciado("El espacio donde trabajo es comodo?");
 		objPregunta5.setObjTipoPregunta(objTipoPregunta2);
 		preguntasCuestionario2.add(objPregunta5);
 
 		Pregunta objPregunta6 = new Pregunta();
-		objPregunta6.setEnunciado("Mi trabajo me exige hacer mucho esfuerzo físico");
+		objPregunta6.setEnunciado("Mi trabajo me exige hacer mucho esfuerzo físico?");
 		objPregunta6.setObjTipoPregunta(objTipoPregunta2);
 		preguntasCuestionario2.add(objPregunta6);
-		
+
 		objCuestionario2.setPreguntas(preguntasCuestionario2);
 		objPregunta4.setObjCuestionario(objCuestionario2);
 		objPregunta5.setObjCuestionario(objCuestionario2);
@@ -165,7 +165,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 
 		this.servicioBDCuestionario.save(objCuestionario2);
 
-		/**------------------------------------------------------- */
+		/** ------------------------------------------------------- */
 		Cuestionario objCuestionario3 = new Cuestionario();
 		objCuestionario3.setTitulo("Cuestionario de factores psicosociales extralaborales");
 		objCuestionario3.setDescripcion("Cuestionario sobre los factores psicosociales extralaborales");
@@ -178,20 +178,20 @@ public class AsstTallerApplication implements CommandLineRunner {
 		List<Pregunta> preguntasCuestionario3 = new ArrayList<>();
 
 		Pregunta objPregunta7 = new Pregunta();
-		objPregunta7.setEnunciado("Es fácil transportarme entre mi casa y el trabajo");
+		objPregunta7.setEnunciado("Es fácil transportarme entre mi casa y el trabajo?");
 		objPregunta7.setObjTipoPregunta(objTipoPregunta3);
 		preguntasCuestionario3.add(objPregunta7);
 
 		Pregunta objPregunta8 = new Pregunta();
-		objPregunta8.setEnunciado("La zonda donde vivo es segura");
+		objPregunta8.setEnunciado("La zonda donde vivo es segura?");
 		objPregunta8.setObjTipoPregunta(objTipoPregunta3);
 		preguntasCuestionario3.add(objPregunta8);
 
 		Pregunta objPregunta9 = new Pregunta();
-		objPregunta9.setEnunciado("Las condiciones de mi vivienda son buenas");
+		objPregunta9.setEnunciado("Las condiciones de mi vivienda son buenas?");
 		objPregunta9.setObjTipoPregunta(objTipoPregunta3);
 		preguntasCuestionario3.add(objPregunta9);
-		
+
 		objCuestionario3.setPreguntas(preguntasCuestionario3);
 		objPregunta7.setObjCuestionario(objCuestionario3);
 		objPregunta8.setObjCuestionario(objCuestionario3);
@@ -199,7 +199,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 
 		this.servicioBDCuestionario.save(objCuestionario3);
 
-		/**------------------------------------------------------- */
+		/** ------------------------------------------------------- */
 		Cuestionario objCuestionario4 = new Cuestionario();
 		objCuestionario4.setTitulo("Cuestionario para la evaluación del estres");
 		objCuestionario4.setDescripcion("Cuestionario sobre la evaluación del estres");
@@ -212,17 +212,17 @@ public class AsstTallerApplication implements CommandLineRunner {
 		List<Pregunta> preguntasCuestionario4 = new ArrayList<>();
 
 		Pregunta objPregunta10 = new Pregunta();
-		objPregunta10.setEnunciado("Dolor de cabeza");
+		objPregunta10.setEnunciado("Dolor de cabeza?");
 		objPregunta10.setObjTipoPregunta(objTipoPregunta4);
 		preguntasCuestionario4.add(objPregunta10);
 
 		Pregunta objPregunta11 = new Pregunta();
-		objPregunta11.setEnunciado("Problemas respiratorios");
+		objPregunta11.setEnunciado("Problemas respiratorios?");
 		objPregunta11.setObjTipoPregunta(objTipoPregunta4);
 		preguntasCuestionario4.add(objPregunta11);
 
 		Pregunta objPregunta12 = new Pregunta();
-		objPregunta12.setEnunciado("Cambios fuertes en el apetito");
+		objPregunta12.setEnunciado("Cambios fuertes en el apetito?");
 		objPregunta12.setObjTipoPregunta(objTipoPregunta4);
 		preguntasCuestionario4.add(objPregunta12);
 
@@ -231,7 +231,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 		objPregunta11.setObjCuestionario(objCuestionario4);
 		objPregunta12.setObjCuestionario(objCuestionario4);
 
-		this.servicioBDCuestionario.save(objCuestionario4); 
+		this.servicioBDCuestionario.save(objCuestionario4);
 	}
 
 	/**
@@ -247,7 +247,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 	private void eliminarPregunta() {
 		Integer idEliminar = 1;
 		this.servicioBDPreguntas.deleteById(idEliminar);
-		if(this.servicioBDPreguntas.findById(idEliminar) == null) {
+		if (this.servicioBDPreguntas.findById(idEliminar) == null) {
 			System.out.println("La pregunta con el id: " + idEliminar + " se elimino con exito.");
 		}
 
@@ -290,7 +290,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 		objTelefono1.setNumero("3234703101");
 		objDocente1.setObjTelefono(objTelefono1);
 		objTelefono1.setObjPersona(objDocente1);
-		
+
 		List<Departamento> departamentosDocente1 = new ArrayList<>();
 		departamentosDocente1.add(objDepartamento1);
 		departamentosDocente1.add(objDepartamento3);
@@ -298,7 +298,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 
 		this.servicioBDDocentes.save(objDocente1);
 
-		/**------------------------------------------------------- */
+		/** ------------------------------------------------------- */
 		Docente objDocente2 = new Docente();
 		objDocente2.setTipoIdentificacion(TipoIdentificacion.CC);
 		objDocente2.setNumeroIdentificacion("1061811002");
@@ -312,7 +312,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 		objTelefono2.setNumero("3234703102");
 		objDocente2.setObjTelefono(objTelefono2);
 		objTelefono2.setObjPersona(objDocente2);
-		
+
 		List<Departamento> departamentosDocente2 = new ArrayList<>();
 		departamentosDocente2.add(objDepartamento1);
 		departamentosDocente2.add(objDepartamento2);
@@ -321,7 +321,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 
 		this.servicioBDDocentes.save(objDocente2);
 
-		/**------------------------------------------------------- */
+		/** ------------------------------------------------------- */
 		Docente objDocente3 = new Docente();
 		objDocente3.setTipoIdentificacion(TipoIdentificacion.CC);
 		objDocente3.setNumeroIdentificacion("1061811003");
@@ -335,7 +335,7 @@ public class AsstTallerApplication implements CommandLineRunner {
 		objTelefono3.setNumero("30003103");
 		objDocente3.setObjTelefono(objTelefono3);
 		objTelefono3.setObjPersona(objDocente3);
-		
+
 		List<Departamento> departamentosDocente3 = new ArrayList<>();
 		departamentosDocente3.add(objDepartamento2);
 		departamentosDocente3.add(objDepartamento3);
@@ -345,12 +345,17 @@ public class AsstTallerApplication implements CommandLineRunner {
 	}
 
 	private void eliminarDocente() {
-		this.servicioBDDocentes.deleteById(2);;
+		this.servicioBDDocentes.deleteById(2);
+		;
 	}
 
 	private void almacenarRespuestas() {
+		/* Traemos al primero docente */
 		Docente objDocente1 = this.servicioBDDocentes.findById(1).get();
+		/* Traemos al segundo docente */
+		Docente objDocente2 = this.servicioBDDocentes.findById(2).get();
 
+		/* Instanciamos las preguntas */
 		Pregunta objPregunta1 = this.servicioBDPreguntas.findById(1).get();
 		Pregunta objPregunta2 = this.servicioBDPreguntas.findById(2).get();
 		Pregunta objPregunta3 = this.servicioBDPreguntas.findById(3).get();
@@ -363,8 +368,8 @@ public class AsstTallerApplication implements CommandLineRunner {
 		Pregunta objPregunta10 = this.servicioBDPreguntas.findById(10).get();
 		Pregunta objPregunta11 = this.servicioBDPreguntas.findById(11).get();
 		Pregunta objPregunta12 = this.servicioBDPreguntas.findById(12).get();
-		
 
+		/* Creación de respuestas para el primer docente */
 		Respuesta objRespuesta1Docente1 = new Respuesta();
 		objRespuesta1Docente1.setDescripcion("5 años");
 		objRespuesta1Docente1.setObjDocente(objDocente1);
@@ -400,6 +405,43 @@ public class AsstTallerApplication implements CommandLineRunner {
 		objRespuesta6Docente1.setObjDocente(objDocente1);
 		objRespuesta6Docente1.setObjPregunta(objPregunta6);
 		this.servicioBDRespuestas.save(objRespuesta6Docente1);
+
+		/* Creación de respuestas para el segundo docente */
+		Respuesta objRespuesta1Docente2 = new Respuesta();
+		objRespuesta1Docente2.setDescripcion("8 años");
+		objRespuesta1Docente2.setObjDocente(objDocente2);
+		objRespuesta1Docente2.setObjPregunta(objPregunta1);
+		this.servicioBDRespuestas.save(objRespuesta1Docente2);
+
+		Respuesta objRespuesta2Docente2 = new Respuesta();
+		objRespuesta2Docente2.setDescripcion("Desarrollador Senior");
+		objRespuesta2Docente2.setObjDocente(objDocente2);
+		objRespuesta2Docente2.setObjPregunta(objPregunta2);
+		this.servicioBDRespuestas.save(objRespuesta2Docente2);
+
+		Respuesta objRespuesta3Docente2 = new Respuesta();
+		objRespuesta3Docente2.setDescripcion("Ingeniero de software");
+		objRespuesta3Docente2.setObjDocente(objDocente2);
+		objRespuesta3Docente2.setObjPregunta(objPregunta3);
+		this.servicioBDRespuestas.save(objRespuesta3Docente2);
+
+		Respuesta objRespuesta4Docente2 = new Respuesta();
+		objRespuesta4Docente2.setDescripcion("No me molesta");
+		objRespuesta4Docente2.setObjDocente(objDocente2);
+		objRespuesta4Docente2.setObjPregunta(objPregunta4);
+		this.servicioBDRespuestas.save(objRespuesta4Docente2);
+
+		Respuesta objRespuesta5Docente2 = new Respuesta();
+		objRespuesta5Docente2.setDescripcion("No, es algo incomodo.");
+		objRespuesta5Docente2.setObjDocente(objDocente2);
+		objRespuesta5Docente2.setObjPregunta(objPregunta5);
+		this.servicioBDRespuestas.save(objRespuesta5Docente1);
+
+		Respuesta objRespuesta6Docente2 = new Respuesta();
+		objRespuesta6Docente2.setDescripcion("Nunca");
+		objRespuesta6Docente2.setObjDocente(objDocente2);
+		objRespuesta6Docente2.setObjPregunta(objPregunta6);
+		this.servicioBDRespuestas.save(objRespuesta6Docente2);
 	}
 
 }
